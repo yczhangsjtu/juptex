c6e9585e5444103c54c9e266b4f5d7ea07315e78
Yuncong Zhang <yczhangsjtu@gmail.com>
2022-12-25 12:50:22 +0800

    Basic editing functions.

--- gui/table_editor.py
+++ gui/table_editor.py
@@ -60,15 +60,40 @@ while running:
             elif event.key == pygame.K_i:
                 editing = True
                 merging = False
-            elif event.key == pygame.K_RETURN and selected_cells and selected_cells[0].row == len(table) - 1:
-                added_row_index = len(table)
-                selected_col = selected_cells[0].col
-                table.append([Cell(added_row_index, col, cell_width, cell_height) for col in range(len(table[0]))])
-                selected_cells = [table[added_row_index][selected_col]]
-                for row in table:
-                    for cell in row:
-                        cell.unset_selected()
-                table[added_row_index][selected_col].set_selected()
+            elif event.key == pygame.K_RETURN and selected_cells:
+                if selected_cells[0].row == len(table) - 1 and pygame.key.get_mods() == 0:
+                    added_row_index = len(table)
+                    selected_col = selected_cells[0].col
+                    table.append([Cell(added_row_index, col, cell_width, cell_height) for col in range(len(table[0]))])
+                    selected_cells = [table[added_row_index][selected_col]]
+                    for row in table:
+                        for cell in row:
+                            cell.unset_selected()
+                    table[added_row_index][selected_col].set_selected()
+                elif pygame.key.get_mods() & pygame.KMOD_CTRL:
+                    selected_row = selected_cells[0].row
+                    selected_col = selected_cells[0].col
+                    table[:] = table[:selected_row+1] + [[Cell(selected_row+1, col, cell_width, cell_height) for col in range(len(table[0]))]] + table[selected_row+1:]
+                    for row_index, row in enumerate(table):
+                        for cell in row:
+                            cell.row = row_index
+                    selected_cells = [table[selected_row+1][selected_col]]
+                    for row in table:
+                        for cell in row:
+                            cell.unset_selected()
+                    selected_cells[0].set_selected()
+                elif pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                    selected_row = selected_cells[0].row
+                    selected_col = selected_cells[0].col
+                    table[:] = table[:selected_row] + [[Cell(selected_row, col, cell_width, cell_height) for col in range(len(table[0]))]] + table[selected_row:]
+                    for row_index, row in enumerate(table):
+                        for cell in row:
+                            cell.row = row_index
+                    selected_cells = [table[selected_row][selected_col]]
+                    for row in table:
+                        for cell in row:
+                            cell.unset_selected()
+                    selected_cells[0].set_selected()
             # Stop the cell merging process if "Esc" is pressed
             elif event.key == pygame.K_ESCAPE:
                 merging = False
@@ -76,18 +101,65 @@ while running:
                 for row in table:
                     for cell in row:
                         cell.unset_selected()
-            elif event.key == pygame.K_TAB and selected_cells and selected_cells[0].col == len(table[0]) - 1:
-                col = len(table[0])
-                selected_row = selected_cells[0].row
-                for row_index, row in enumerate(table):
-                    row.append(Cell(row_index, col, cell_width, cell_height))
-                selected_cells = [table[selected_cells[0].row][col]]
-                for row in table:
-                    for cell in row:
-                        cell.unset_selected()
-                table[selected_row][-1].set_selected()
+            elif event.key == pygame.K_TAB and selected_cells:
+                if selected_cells[0].col == len(table[0]) - 1 and pygame.key.get_mods() == 0:
+                    col = len(table[0])
+                    selected_row = selected_cells[0].row
+                    for row_index, row in enumerate(table):
+                        row.append(Cell(row_index, col, cell_width, cell_height))
+                    selected_cells = [table[selected_cells[0].row][col]]
+                    for row in table:
+                        for cell in row:
+                            cell.unset_selected()
+                    table[selected_row][-1].set_selected()
+                elif pygame.key.get_mods() & pygame.KMOD_CTRL:
+                    selected_row = selected_cells[0].row
+                    selected_col = selected_cells[0].col
+                    for row_index, row in enumerate(table):
+                        row[:] = row[:selected_col+1] + [Cell(row_index, selected_col+1, cell_width, cell_height)] + row[selected_col+1:]
+                        for col_index in range(selected_col+2, len(row)):
+                            row[col_index].col = col_index
+                    selected_cells = [table[selected_row][selected_col+1]]
+                    for row in table:
+                        for cell in row:
+                            cell.unset_selected()
+                    selected_cells[0].set_selected()
+                elif pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                    selected_row = selected_cells[0].row
+                    selected_col = selected_cells[0].col
+                    for row_index, row in enumerate(table):
+                        row[:] = row[:selected_col] + [Cell(row_index, selected_col, cell_width, cell_height)] + row[selected_col:]
+                        for col_index in range(selected_col+1, len(row)):
+                            row[col_index].col = col_index
+                    selected_cells = [table[selected_row][selected_col]]
+                    for row in table:
+                        for cell in row:
+                            cell.unset_selected()
+                    selected_cells[0].set_selected()
+            elif event.key == pygame.K_d and selected_cells:
+                if pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                    if len(table) > 1:
+                        selected_row = selected_cells[0].row
+                        del table[selected_row]
+                        for row_index, row in enumerate(table):
+                            for cell in row:
+                                cell.row = row_index
+                        selected_cells = []
+                        for row in table:
+                            for cell in row:
+                                cell.unset_selected()
+                elif len(table[0]) > 1:
+                    selected_col = selected_cells[0].col
+                    for row in table:
+                        del row[selected_col]
+                        for col_index, cell in enumerate(row):
+                            cell.col = col_index
+                    selected_cells = []
+                    for row in table:
+                        for cell in row:
+                            cell.unset_selected()
 
-        # Handle mouse events# Handle mouse events
+        # Handle mouse events
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # Get the mouse position
             mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -124,6 +196,9 @@ while running:
         elif event.type == pygame.QUIT:
             running = False
 
+
+    # Draw the cell background
+    pygame.draw.rect(screen, (255, 255, 255), (0, 0, *window_size))
     # Draw the table
     for row in table:
         for cell in row:
