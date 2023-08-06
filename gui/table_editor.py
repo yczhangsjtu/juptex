1671fd430250b88fd3c808c9950092332ae58988
Yuncong Zhang <yczhangsjtu@gmail.com>
2023-08-06 14:30:27 +0800

    Update table_editor.py

--- gui/table_editor.py
+++ gui/table_editor.py
@@ -24,32 +24,79 @@ table = [[Cell(row, col, cell_width, cell_height)
          for row in range(3)]
 
 
-def get_border_select_state():
-    vertical_border_select_state = [0 for i in range(len(table[0])+1)]
-    horizontal_border_select_state = [0 for i in range(len(table)+1)]
-    for i in range(len(table[0])+1):
-        vselect = [(i > 0 and row[i-1].selected_right_border) or
-                   (i < len(table[0]) and row[i].selected_left_border)
-                   for row in table]
+def get_table_height():
+    return len(table)
+
+
+def get_table_width():
+    return len(table[0])
+
+def num_horizontal_borders():
+    return get_table_height() + 1
+
+def num_vertical_borders():
+    return get_table_width() + 1
+
+
+NON_SELECTED = 0
+PART_SELECTED = 1
+ALL_SELECTED = 2
+
+
+def vertical_border_selected(row, index):
+    assert index >= 0 and index <= get_table_width(), f"Index out of range, {get_table_width()} columns, index {index}"
+    return (index > 0 and row[index-1].selected_right_border) or \
+           (index < get_table_width() and row[index].selected_left_border)
+
+
+def horizontal_border_selected(col_index, index):
+    assert col_index >= 0 and col_index < get_table_width(), f"Index out of range, {get_table_width()} columns, index {index}"
+    return (index > 0 and table[index-1][col_index].selected_bottom_border) or \
+           (index < get_table_height() and table[index][col_index].selected_top_border)
+
+
+def vertical_borders_selected():
+    vertical_border_select_state = [NON_SELECTED for i in range(get_table_width()+1)]
+    for i in range(num_vertical_borders()):
+        vselect = [vertical_border_selected(row, i) for row in table]
         if all(vselect):
-            vertical_border_select_state[i] = 2
+            vertical_border_select_state[i] = ALL_SELECTED
         elif any(vselect):
-            vertical_border_select_state[i] = 1
-    for i in range(len(table)+1):
-        hselect = [(i > 0 and table[i-1][j].selected_bottom_border) or
-                   (i < len(table) and table[i][j].selected_top_border)
-                   for j in range(len(table[0]))]
+            vertical_border_select_state[i] = PART_SELECTED
+    return vertical_border_select_state
+
+
+def horizontal_borders_selected():
+    horizontal_border_select_state = [NON_SELECTED for i in range(get_table_height()+1)]
+    for i in range(num_horizontal_borders()):
+        hselect = [horizontal_border_selected(j, i) for j in range(get_table_width())]
         if all(hselect):
-            horizontal_border_select_state[i] = 2
+            horizontal_border_select_state[i] = ALL_SELECTED
         elif any(hselect):
-            horizontal_border_select_state[i] = 1
-    horizontal_selector_rect = [(Cell.xshift - 20,
+            horizontal_border_select_state[i] = PART_SELECTED
+    return horizontal_border_select_state
+
+
+def horizontal_selector_rect(index):
+    assert index >= 0 and index <= get_table_height(), f"Index out of range, {get_table_height()} rows, index {index}"
+    return (Cell.xshift - 20,
             Cell.yshift + i * (cell_height+Cell.distance) - Cell.distance//2 - 6,
-            12, 12) for i in range(len(table)+1)]
-    vertical_selector_rect = [(Cell.xshift + i * (cell_width+Cell.distance) - Cell.distance//2 - 6,
-                Cell.yshift - 20,
-                12, 12) for i in range(len(table[0])+1)]
-    return horizontal_border_select_state, vertical_border_select_state, horizontal_selector_rect, vertical_selector_rect
+            12,
+            12)
+
+
+def vertical_selector_rect(index):
+    assert index >= 0 and index <= get_table_width(), f"Index out of range, {get_table_width()} columns, index {index}"
+    return (Cell.xshift + i * (cell_width+Cell.distance) - Cell.distance//2 - 6,
+            Cell.yshift - 20,
+            12,
+            12)
+
+
+def get_border_select_state():
+    vertical_border_select_state = vertical_borders_selected()
+    horizontal_border_select_state = horizontal_borders_selected()
+    return horizontal_border_select_state, vertical_border_select_state
 
 
 # Create the selected cells list
@@ -62,7 +109,7 @@ merging = False
 # Run the game loop
 running = True
 while running:
-    horizontal_border_select_state, vertical_border_select_state, horizontal_selector_rect, vertical_selector_rect = get_border_select_state()
+    horizontal_border_select_state, vertical_border_select_state = get_border_select_state()
     for event in pygame.event.get():
         # Handle keyboard events
         if event.type == pygame.KEYDOWN:
@@ -92,10 +139,10 @@ while running:
                 merging = False
             elif event.key == pygame.K_RETURN:
                 if selected_cells:
-                    if selected_cells[0].row == len(table) - 1 and pygame.key.get_mods() == 0:
-                        added_row_index = len(table)
+                    if selected_cells[0].row == get_table_height() - 1 and pygame.key.get_mods() == 0:
+                        added_row_index = get_table_height()
                         selected_col = selected_cells[0].col
-                        table.append([Cell(added_row_index, col, cell_width, cell_height) for col in range(len(table[0]))])
+                        table.append([Cell(added_row_index, col, cell_width, cell_height) for col in range(get_table_width())])
                         selected_cells = [table[added_row_index][selected_col]]
                         for row in table:
                             for cell in row:
@@ -104,7 +151,7 @@ while running:
                     elif pygame.key.get_mods() & pygame.KMOD_CTRL:
                         selected_row = selected_cells[0].row
                         selected_col = selected_cells[0].col
-                        table[:] = table[:selected_row+1] + [[Cell(selected_row+1, col, cell_width, cell_height) for col in range(len(table[0]))]] + table[selected_row+1:]
+                        table[:] = table[:selected_row+1] + [[Cell(selected_row+1, col, cell_width, cell_height) for col in range(get_table_width())]] + table[selected_row+1:]
                         for row_index, row in enumerate(table):
                             for cell in row:
                                 cell.row = row_index
@@ -116,7 +163,7 @@ while running:
                     elif pygame.key.get_mods() & pygame.KMOD_SHIFT:
                         selected_row = selected_cells[0].row
                         selected_col = selected_cells[0].col
-                        table[:] = table[:selected_row] + [[Cell(selected_row, col, cell_width, cell_height) for col in range(len(table[0]))]] + table[selected_row:]
+                        table[:] = table[:selected_row] + [[Cell(selected_row, col, cell_width, cell_height) for col in range(get_table_width())]] + table[selected_row:]
                         for row_index, row in enumerate(table):
                             for cell in row:
                                 cell.row = row_index
@@ -148,8 +195,8 @@ while running:
                     for cell in row:
                         cell.unset_selected()
             elif event.key == pygame.K_TAB and selected_cells:
-                if selected_cells[0].col == len(table[0]) - 1 and pygame.key.get_mods() == 0:
-                    col = len(table[0])
+                if selected_cells[0].col == get_table_width() - 1 and pygame.key.get_mods() == 0:
+                    col = get_table_width()
                     selected_row = selected_cells[0].row
                     for row_index, row in enumerate(table):
                         row.append(Cell(row_index, col, cell_width, cell_height))
@@ -184,7 +231,7 @@ while running:
                     selected_cells[0].set_selected()
             elif event.key == pygame.K_d and selected_cells:
                 if pygame.key.get_mods() & pygame.KMOD_SHIFT:
-                    if len(table) > 1:
+                    if get_table_height() > 1:
                         selected_row = selected_cells[0].row
                         del table[selected_row]
                         for row_index, row in enumerate(table):
@@ -194,7 +241,7 @@ while running:
                         for row in table:
                             for cell in row:
                                 cell.unset_selected()
-                elif len(table[0]) > 1:
+                elif get_table_width() > 1:
                     selected_col = selected_cells[0].col
                     for row in table:
                         del row[selected_col]
@@ -232,40 +279,40 @@ while running:
                     elif cell.is_on_bottom_border(mouse_x, mouse_y):
                         cell.toggle_selected_bottom_border()
             
-            for i in range(len(table)+1):
-                x, y, w, h = horizontal_selector_rect[i]
+            for i in range(get_table_height()+1):
+                x, y, w, h = horizontal_selector_rect(i)
                 if x <= mouse_x < x+w and y <= mouse_y < y+h:
-                    if horizontal_border_select_state[i] < 2:
+                    if horizontal_border_select_state[i] < ALL_SELECTED:
                         if i > 0:
                             for cell in table[i-1]:
                                 cell.set_selected_bottom_border()
-                        if i < len(table):
+                        if i < get_table_height():
                             for cell in table[i]:
                                 cell.set_selected_top_border()
                     else:
                         if i > 0:
                             for cell in table[i-1]:
                                 cell.unset_selected_bottom_border()
-                        if i < len(table):
+                        if i < get_table_height():
                             for cell in table[i]:
                                 cell.unset_selected_top_border()
 
-            for i in range(len(table[0])+1):
-                x, y, w, h = vertical_selector_rect[i]
+            for i in range(get_table_width()+1):
+                x, y, w, h = vertical_selector_rect(i)
                 if x <= mouse_x < x+w and y <= mouse_y < y+h:
-                    if vertical_border_select_state[i] < 2:
+                    if vertical_border_select_state[i] < ALL_SELECTED:
                         if i > 0:
-                            for j in range(len(table)):
+                            for j in range(get_table_height()):
                                 table[j][i-1].set_selected_right_border()
-                        if i < len(table[0]):
-                            for j in range(len(table)):
+                        if i < get_table_width():
+                            for j in range(get_table_height()):
                                 table[j][i].set_selected_left_border()
                     else:
                         if i > 0:
-                            for j in range(len(table)):
+                            for j in range(get_table_height()):
                                 table[j][i-1].unset_selected_right_border()
-                        if i < len(table[0]):
-                            for j in range(len(table)):
+                        if i < get_table_width():
+                            for j in range(get_table_height()):
                                 table[j][i].unset_selected_left_border()
 
         # Handle mouse movement events
@@ -295,31 +342,31 @@ while running:
                      (255, 255, 255),
                      (Cell.xshift-Cell.distance,
                       Cell.yshift-Cell.distance,
-                      len(table[0]) * (cell_width+Cell.distance) + Cell.distance,
-                      len(table) * (cell_height+Cell.distance) + Cell.distance))
+                      get_table_width() * (cell_width+Cell.distance) + Cell.distance,
+                      get_table_height() * (cell_height+Cell.distance) + Cell.distance))
     # Draw the table
     for row in table:
         for cell in row:
             cell.draw(screen, editing)
     
     # Draw the border selectors
-    horizontal_border_select_state, vertical_border_select_state, horizontal_selector_rect, vertical_selector_rect = get_border_select_state()
-    for i in range(len(table) + 1):
+    horizontal_border_select_state, vertical_border_select_state = get_border_select_state()
+    for i in range(get_table_height() + 1):
         pygame.draw.rect(screen,
                          [(255, 255, 255),
                           (127, 127, 127),
                           (0, 0, 255)][horizontal_border_select_state[i]],
-                         horizontal_selector_rect[i], border_radius=3)
+                         horizontal_selector_rect(i), border_radius=3)
         pygame.draw.rect(screen, (0, 0, 0),
-                         horizontal_selector_rect[i], width=1, border_radius=3)
-    for i in range(len(table[0]) + 1):
+                         horizontal_selector_rect(i), width=1, border_radius=3)
+    for i in range(get_table_width() + 1):
         pygame.draw.rect(screen,
                          [(255, 255, 255),
                           (127, 127, 127),
                           (0, 0, 255)][vertical_border_select_state[i]],
-                         vertical_selector_rect[i], border_radius=3)
+                         vertical_selector_rect(i), border_radius=3)
         pygame.draw.rect(screen, (0, 0, 0),
-                         vertical_selector_rect[i], width=1, border_radius=3)
+                         vertical_selector_rect(i), width=1, border_radius=3)
 
     # Update the display
     pygame.display.flip()
@@ -327,7 +374,7 @@ while running:
 
 def export_to_latex():
     # Initialize the LaTeX code
-    latex = "\\begin{tabular}{|" + "c|"*len(table[0]) + "}\n"
+    latex = "\\begin{tabular}{|" + "c|"*get_table_width() + "}\n"
 
     # Add the contents of each cell to the LaTeX code
     for row in table:
