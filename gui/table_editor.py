39085769d6976623471ed1c59b6ea1d07573a378
Yuncong Zhang <yczhangsjtu@gmail.com>
2023-08-27 17:30:10 +0800

    Refactor.

--- gui/table_editor.py
+++ gui/table_editor.py
@@ -64,12 +64,12 @@ class Table:
     def num_vertical_borders(self):
         return self.get_table_width() + 1
 
-    def vertical_border_selected(self, row, index):
+    def _vertical_border_selected(self, row, index):
         assert index >= 0 and index <= self.get_table_width(), f"Index out of range, {self.get_table_width()} columns, index {index}"
         return (index > 0 and row[index-1].selected_right_border) or \
                (index < self.get_table_width() and row[index].selected_left_border)
 
-    def horizontal_border_selected(self, col_index, index):
+    def _horizontal_border_selected(self, col_index, index):
         assert col_index >= 0 and col_index < self.get_table_width(), f"Index out of range, {self.get_table_width()} columns, index {index}"
         return (index > 0 and self.cells[index-1][col_index].selected_bottom_border) or \
                (index < self.get_table_height() and self.cells[index][col_index].selected_top_border)
@@ -77,17 +77,16 @@ class Table:
     def vertical_borders_selected(self):
         vertical_border_select_state = [NON_SELECTED for i in range(self.get_table_width()+1)]
         for i in range(self.num_vertical_borders()):
-            vselect = [self.vertical_border_selected(row, i) for row in self.cells]
+            vselect = [self._vertical_border_selected(row, i) for row in self.cells]
             vertical_border_select_state[i] = multiple_select_state(vselect)
         return vertical_border_select_state
 
     def horizontal_borders_selected(self):
         horizontal_border_select_state = [NON_SELECTED for i in range(self.get_table_height()+1)]
         for i in range(self.num_horizontal_borders()):
-            hselect = [self.horizontal_border_selected(j, i) for j in range(self.get_table_width())]
+            hselect = [self._horizontal_border_selected(j, i) for j in range(self.get_table_width())]
             horizontal_border_select_state[i] = multiple_select_state(hselect)
         return horizontal_border_select_state
-
 
     def horizontal_selector_rect(self, index):
         assert index >= 0 and index <= self.get_table_height(), f"Index out of range, {self.get_table_height()} rows, index {index}"
@@ -96,7 +95,6 @@ class Table:
                 WHOLE_BORDER_SELECTOR_SIZE,
                 WHOLE_BORDER_SELECTOR_SIZE)
 
-
     def vertical_selector_rect(self, index):
         assert index >= 0 and index <= self.get_table_width(), f"Index out of range, {self.get_table_width()} columns, index {index}"
         return (Cell.xshift + index * (self.cell_width + Cell.distance) - (Cell.distance + WHOLE_BORDER_SELECTOR_SIZE)//2,
@@ -104,13 +102,11 @@ class Table:
                 WHOLE_BORDER_SELECTOR_SIZE,
                 WHOLE_BORDER_SELECTOR_SIZE)
 
-
     def cell_region_rect(self):
         return (Cell.xshift-Cell.distance,
                 Cell.yshift-Cell.distance,
                 self.get_table_width() * (self.cell_width+Cell.distance) + Cell.distance,
                 self.get_table_height() * (self.cell_height+Cell.distance) + Cell.distance)
-
 
     def draw_border_selectors(self):
         # Draw the border selectors
@@ -127,70 +123,60 @@ class Table:
             pygame.draw.rect(screen, (0, 0, 0),
                              self.vertical_selector_rect(i), width=1, border_radius=3)
 
-
     def edit_selected_cell(self, event, selected_cell):
         # Update the cell text with the typed character
         if event.unicode.isprintable():
-            selected_cell.set_text(
-                selected_cell.text + event.unicode)
+            selected_cell.set_text(selected_cell.text + event.unicode)
         elif event.key == pygame.K_BACKSPACE:
             if len(selected_cell.text) > 0:
                 selected_cell.set_text(selected_cell.text[:-1])
+    
+    def insert_row_at_index(self, index):
+        assert index >= 0 and index <= self.get_table_height(), f"Index out of range, {self.get_table_height()} rows, index {index}"
+        self.cells.insert(index, [Cell(index, col, self.cell_width, self.cell_height)
+                                  for col in range(self.get_table_width())])
+        for row in self.cells[index+1:]:
+            for cell in row:
+                cell.row += 1
+    
+    def insert_column_at_index(self, index):
+        assert index >= 0 and index <= self.get_table_width(), f"Index out of range, {self.get_table_width()} columns, index {index}"
+        for row in self.cells:
+            row.insert(index, Cell(row[0].row, index, self.cell_width, self.cell_height))
+            for cell in row[index+1:]:
+                cell.col += 1
 
     def add_row_at_bottom(self):
-        added_row_index = self.get_table_height()
-        self.cells.append([Cell(added_row_index, col, self.cell_width, self.cell_height) for col in range(self.get_table_width())])
-        self.selected_cells = [self.cells[added_row_index][0]]
+        self.insert_row_at_index(self.get_table_height())
+    
+    def add_row_at_top(self):
+        self.insert_row_at_index(0)
+    
+    def add_column_at_right(self):
+        self.insert_column_at_index(self.get_table_width())
+    
+    def add_column_at_left(self):
+        self.insert_column_at_index(0)
 
-    def insert_row_below(self):
+    def insert_row_below_selected(self):
         selected_row = self.selected_cells[0].row
         selected_col = self.selected_cells[0].col
-        self.cells[:] = self.cells[:selected_row+1] + [[Cell(selected_row+1, col, self.cell_width, self.cell_height) for col in range(self.get_table_width())]] + self.cells[selected_row+1:]
-        for row_index in range(selected_row+1, self.get_table_height()):
-            for cell in self.cells[row_index]:
-                cell.row += 1
-        self.selected_cells = [self.cells[selected_row+1][0]]
+        self.insert_row_at_index(selected_row+1)
+        self.selected_cells = [self.cells[selected_row+1][selected_col]]
 
-    def insert_row_above(self):
+    def insert_row_above_selected(self):
+        selected_row = self.selected_cells[0].row
+        self.insert_row_at_index(selected_row)
+    
+    def insert_column_left_selected(self):
         selected_row = self.selected_cells[0].row
         selected_col = self.selected_cells[0].col
-        self.cells[:] = self.cells[:selected_row] + [[Cell(selected_row, col, cell_width, cell_height) for col in range(self.get_table_width())]] + self.cells[selected_row:]
-        for row_index, row in enumerate(self.cells):
-            for cell in row:
-                cell.row = row_index
-        self.selected_cells = [self.cells[selected_row][selected_col]]
-
-    def insert_column_at_selected(self):
-        """Insert a column at the left or right of the selected cells."""
-
-        selected_row = self.selected_cells[0].row
-
-        # Case 1: Insert a new column at the end of the table if the right-most is selected.
-        if self.selected_cells[0].col == self.get_table_width() - 1 and pygame.key.get_mods() == 0:
-            col = self.get_table_width()
-            for row_index, row in enumerate(self.cells):
-                row.append(Cell(row_index, col, self.cell_width, self.cell_height))
-
-        # Case 2: Insert a new column to the right of the selected cells, when Ctrl is pressed.
-        elif pygame.key.get_mods() & pygame.KMOD_CTRL:
-            for row_index, row in enumerate(self.cells):
-                row[:] = row[:self.selected_cells[0].col+1] + [Cell(row_index, self.selected_cells[0].col+1, self.cell_width, self.cell_height)] + row[self.selected_cells[0].col+1:]
-                for col_index in range(self.selected_cells[0].col+2, len(row)):
-                    row[col_index].col = col_index
-
-        # Case 3: Insert a new column to the left of the selected cells, when Shift is pressed.
-        elif pygame.key.get_mods() & pygame.KMOD_SHIFT:
-            for row_index, row in enumerate(self.cells):
-                row[:] = row[:self.selected_cells[0].col] + [Cell(row_index, self.selected_cells[0].col, self.cell_width, self.cell_height)] + row[self.selected_cells[0].col:]
-                for col_index in range(self.selected_cells[0].col+1, len(row)):
-                    row[col_index].col = col_index
-
-        # Update the selected cells.
-        self.selected_cells = [self.cells[selected_row][self.selected_cells[0].col]]
-        for row in self.cells:
-            for cell in row:
-                cell.unset_selected()
-        self.selected_cells[0].set_selected()
+        self.insert_column_at_index(selected_col)
+        self.selected_cells = [self.cells[selected_row][selected_col+1]]
+    
+    def insert_column_right_selected(self):
+        selected_col = self.selected_cells[0].col
+        self.insert_column_at_index(selected_col+1)
 
     def delete_selected_cells(self):
         if pygame.key.get_mods() & pygame.KMOD_SHIFT:
@@ -275,10 +261,10 @@ def on_press_enter():
         table.add_row_at_bottom()
 
     elif pygame.key.get_mods() & pygame.KMOD_CTRL:
-        table.insert_row_below()
+        table.insert_row_below_selected()
 
     elif pygame.key.get_mods() & pygame.KMOD_SHIFT:
-        table.insert_row_above()
+        table.insert_row_above_selected()
 
     for row in table.cells:
         for cell in row:
@@ -315,7 +301,10 @@ def on_key_down(event):
             for cell in row:
                 cell.unset_selected()
     elif event.key == pygame.K_TAB and table.selected_cells:
-        table.insert_column_at_selected()
+        if pygame.key.get_mods() & pygame.KMOD_SHIFT:
+            table.insert_column_left_selected()
+        else:
+            table.insert_column_right_selected()
     elif event.key == pygame.K_d and table.selected_cells:
         table.delete_selected_cells()
 
